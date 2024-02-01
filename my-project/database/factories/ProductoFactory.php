860c1260a1ebc61cf5e3017b95e55c5e92230c76
Skale21k <zeos21k@gmail.com>
2024-02-01 23:29:14 +0100

    Creada factoria de Producto

---
+++ my-project/database/factories/ProductoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Producto>
+ */
+class ProductoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => fake()->word(),
+            'descripcion' => fake()->paragraph(),
+            'precio' => fake()->numberBetween(1, 99),
+            'unidades' => fake()->numberBetween(1, 99),
+            'imagen' => fake()->imageUrl(),
+            'categoria' => fake()->word(),
+        ];
+    }
+}
