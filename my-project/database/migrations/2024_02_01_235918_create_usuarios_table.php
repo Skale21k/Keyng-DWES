0246bf5d363c382ccc75685725b2f7d13a8cac18
Skale21k <zeos21k@gmail.com>
2024-02-02 01:05:13 +0100

    Creado modelo Usuario y migracion

---
+++ my-project/database/migrations/2024_02_01_235918_create_usuarios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 80);
+            $table->string('email', 80)->unique();
+            $table->string('contraseña', 255);
+            $table->string('direccion', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
