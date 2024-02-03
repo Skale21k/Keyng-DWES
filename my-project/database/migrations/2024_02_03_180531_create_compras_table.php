0562897faefcb1865b0320abedc75eeec8a94fbe
Skale21k <zeos21k@gmail.com>
2024-02-03 19:09:21 +0100

    Creada migración de la tabla Compras

---
+++ my-project/database/migrations/2024_02_03_180531_create_compras_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained();
+            $table->foreignId('producto_id')->constrained();    
+            $table->integer('cantidad');
+            $table->decimal('total', 8, 2);
+            $table->date('fecha_compra');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('compras');
+    }
+};
