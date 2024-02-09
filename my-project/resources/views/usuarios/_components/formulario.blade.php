a8c6509e532238d518467235161ce556604fbe43
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-09 20:24:11 +0100

    Creo formulario para usuarios

---
+++ my-project/resources/views/usuarios/_components/formulario.blade.php
@@ -0,0 +1,17 @@
+<form action="{{ route('usuario.store') }}" method="POST">
+    @csrf
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre" id="nombre">
+    <br>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <br>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <br>
+    <label for="direccion">Unidades</label>
+    <input type="text" name="direccion" id="direccion">
+    <br>
+
+    <button type="submit">Registrarse</button>
+</form>
