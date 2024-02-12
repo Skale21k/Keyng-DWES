2c1da5268c7ecbb1a1d9905c50e250f4580ac6b3
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-12 18:38:35 +0100

    agrego la navegacion

--- my-project/resources/views/layouts/_partials/header.blade.php
+++ my-project/resources/views/layouts/_partials/header.blade.php
@@ -1,24 +1,34 @@
 <header>
-    <nav>
-
-        <ul>
-            <div class="logo">
-                <li><a href=" {{ route('home') }}"> <img src="{{ asset('assets/img/logoBlancoLetras.png') }}"
-                            id="logo" alt=""></a></li>
-            </div>
-        </ul>
-
-        <ul class="utilidad">
-            <li>
-                <div class="formBusqueda">
-                    <form action="{{ route('home') }}" method="GET" id="buscarform">
-                        <input type="text" id="s" value="" placeholder="Buscar" />
-                    </form>
+    <div class="container-nav">
+        <nav>
+            <ul>
+                <div class="logo">
+                    <li><a href=" {{ route('home') }}"> <img src="{{ asset('assets/img/logoBlancoLetras.png') }}"
+                                id="logo" alt=""></a></li>
                 </div>
-            </li>
-            <li><a href=""><img src="{{ asset('assets/img/carrito.png') }}" alt=""></a></li>
-            <li><a href=" {{ route('usuarios.index') }}"> <img src="{{ asset('assets/img/usuario.png') }}"
-                        alt=""></a></li>
+            </ul>
+
+            <ul class="utilidad">
+                <li>
+                    <div class="formBusqueda">
+                        <form action="{{ route('home') }}" method="GET" id="buscarform">
+                            <input type="text" id="s" value="" placeholder="Buscar" />
+                        </form>
+                    </div>
+                </li>
+                <li><a href=""><img src="{{ asset('assets/img/carrito.png') }}" alt=""></a></li>
+                <li><a href=" {{ route('usuarios.index') }}"> <img src="{{ asset('assets/img/usuario.png') }}"
+                            alt=""></a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="navegacion">
+        <nav>
+        <ul>
+            <li>Productos</li>
+            <li>Administración</li>
+            <li id="sobreNosotos">Sobre Nosotros</li>
         </ul>
     </nav>
+    </div>
 </header>
