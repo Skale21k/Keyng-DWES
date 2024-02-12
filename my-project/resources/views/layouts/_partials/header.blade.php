ec254d0864b1ec4a303ad4b7d52f206a6d583451
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-12 17:35:30 +0100

    cambio el logo

--- my-project/resources/views/layouts/_partials/header.blade.php
+++ my-project/resources/views/layouts/_partials/header.blade.php
@@ -2,13 +2,21 @@
     <nav>
 
         <ul>
-            <li><a href=" {{route ('home')}}"></a> <img src="{{ asset('assets/img/logo.png') }}" id="logo" alt=""></li>
+            <li><a href=" {{ route('home') }}"></a> <img src="{{ asset('assets/img/logoBlanco.png') }}" id="logo"
+                    alt=""></li>
         </ul>
 
         <ul class="utilidad">
-
-            <li><a href=""><img src="{{ asset('assets/img/carrito.png')}}" alt=""></a></li>
-            <li><a href=" {{route ('usuarios.index')}}"></a> <img src="{{ asset('assets/img/usuario.png')}}" alt=""></li>
+            <li>
+                <div class="formBusqueda">
+                    <form action="{{ route('home') }}" method="GET" id="buscarform">
+                        <input type="text" id="s" value="" placeholder="Buscar" />
+                    </form>
+                </div>
+            </li>
+            <li><a href=""><img src="{{ asset('assets/img/carrito.png') }}" alt=""></a></li>
+            <li><a href=" {{ route('usuarios.index') }}"></a> <img src="{{ asset('assets/img/usuario.png') }}"
+                    alt=""></li>
         </ul>
     </nav>
 </header>
