b5a028b11097414953b792553e262a851e36a3c4
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-12 16:35:53 +0100

    añado el log y la estructura basica del header

---
+++ my-project/resources/views/layouts/_partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <img src="{{ asset('assets/img/logo.png') }}" alt="">
+        <ul>
+            <li><a href="{{ route('productos.index') }}">Productos</a></li>
+        </ul>
+    </nav>
+</header>
