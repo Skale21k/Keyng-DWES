b1b1937ed5542dd190f63426ffeee221f2e325b2
Sergiksw <sergiokoss120@gmail.com>
2024-02-13 22:04:21 +0100

    hecha vista fea de productos

--- my-project/resources/views/productos/index.blade.php
+++ my-project/resources/views/productos/index.blade.php
@@ -6,14 +6,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Productos</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/productos.css') }}">
 </head>
 <body>
-    <h1>Lista de Productos</h1>
+    <h1>Productos</h1>
 
-    <ul>
+    <div class="productos-container">
         @foreach($productos as $producto)
-            <li>{{ $producto->nombre }} - {{ $producto->precio }}</li>
+            <div class="producto">
+                <img src="{{ asset('assets/img/ImgProductos'.$producto->imagen) }}" alt="{{ $producto->nombre }}">
+                <h3>{{ $producto->nombre }}</h3>
+                <p>Precio: ${{ $producto->precio }}</p>
+            </div>
         @endforeach
-    </ul>
+    </div>
 </body>
 </html>
