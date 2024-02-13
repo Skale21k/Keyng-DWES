975204844efbf016dede688b52170baa5bbf8d6a
Sergiksw <sergiokoss120@gmail.com>
2024-02-13 22:20:28 +0100

    corregido index.blade.php usando plantilla ahora

--- my-project/resources/views/productos/index.blade.php
+++ my-project/resources/views/productos/index.blade.php
@@ -1,24 +1,16 @@
-<!-- resources/views/productos/index.blade.php -->
+@extends('layouts.plantilla')
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Productos</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/productos.css') }}">
-</head>
-<body>
-    <h1>Productos</h1>
+@section('title', 'Lista de Productos')
 
+@section('content')
+    <h1>Lista de Productos</h1>
     <div class="productos-container">
         @foreach($productos as $producto)
             <div class="producto">
-                <img src="{{ asset('assets/img/ImgProductos'.$producto->imagen) }}" alt="{{ $producto->nombre }}">
+                <img src="{{ asset('images/'.$producto->imagen) }}" alt="{{ $producto->nombre }}">
                 <h3>{{ $producto->nombre }}</h3>
                 <p>Precio: ${{ $producto->precio }}</p>
             </div>
         @endforeach
     </div>
-</body>
-</html>
+@endsection
