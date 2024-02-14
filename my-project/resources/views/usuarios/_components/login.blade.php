441bdfd0c016266aef7e93d0001b4e95b65c52cc
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-14 16:10:25 +0100

    agrego el formulario de login

---
+++ my-project/resources/views/usuarios/_components/login.blade.php
@@ -0,0 +1,9 @@
+<form action="{{ route('usuarios.login') }}" method="POST">
+<label for="email">Email</label>
+<input type="email" name="email" id="email">
+<br>
+<label for="password">Password</label>
+<input type="password" name="password" id="password">
+<br>
+<button type="submit">Iniciar Sesion</button>
+</form>
