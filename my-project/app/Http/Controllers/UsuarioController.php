bb31201d937ea9acc7800bc256f9d293ebd9ccfd
Dario Soto <DarioSoto_97@hotmail.com>
2024-02-14 16:44:58 +0100

    Cambiado Usuario por User en controlador

--- my-project/app/Http/Controllers/UsuarioController.php
+++ my-project/app/Http/Controllers/UsuarioController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Usuario;
+use App\Models\User;
 
 
 class UsuarioController extends Controller
@@ -19,7 +19,7 @@ class UsuarioController extends Controller
     }
 
     public function store(Request $request){
-        $p = new Usuario();
+        $p = new User();
         $p->nombre = $request->nombre;
         $p->email = $request->email;
         $p->password = $request->password;
