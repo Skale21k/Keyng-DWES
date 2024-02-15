bd9b3d849aa9147979eac9f86656ec9cc0dbca42
Sergiksw <sergiokoss120@gmail.com>
2024-02-15 16:40:57 +0100

    form usuarios god tier

--- my-project/resources/views/usuarios/create.blade.php
+++ my-project/resources/views/usuarios/create.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.plantilla')
 
 @section('title', 'Registrar Usuario')
+@section('content')
 
 <h2>Registro de usuarios</h2>
 
 @component('usuarios._components.formulario')
 @endcomponent
-
+@endsection
