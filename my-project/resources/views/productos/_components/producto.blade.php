55b7fb7a73e14c96909f6d470dccf1542ebb4408
Sergiksw <sergiokoss120@gmail.com>
2024-02-16 15:09:49 +0100

    Separado a componentes, que se me olvido jeje

--- my-project/resources/views/productos/_components/producto.blade.php
+++ my-project/resources/views/productos/_components/producto.blade.php
@@ -1,11 +1,8 @@
-<body class="bodyH">
-    <div class="contenedorH">
-        <div class="productoH">
-            <img src="{{ $producto->imagen_url }}" alt="{{ $producto->nombre }}">
-            <div class="datosH">
-                <h3>{{ $producto->nombre }}</h3>
-                <p>Precio: ${{ $producto->precio }}</p>
-            </div>
-        </div>
+<div class="productoH">
+    <a href="{{ route('productos.show', $producto) }}" class="aH">
+    <img src="{{ $producto->imagen_url }}" alt="{{ $producto->nombre }}">
+    <div class="datosH">
+        <h3>{{ $producto->nombre }}</h3>
+        <p>Precio: ${{ $producto->precio }}</p>
     </div>
-</body>
+</div>
--- my-project/resources/views/productos/index.blade.php
+++ my-project/resources/views/productos/index.blade.php
@@ -3,17 +3,12 @@
 @section('title', 'Lista de Productos')
 
 @section('content')
-<h1>Lista de Productos</h1>
-<div class="contenedorH">
-    @foreach($productos as $producto)
-        <div class="productoH">
-            <img src="{{ $producto->imagen_url }}" alt="{{ $producto->nombre }}">
-            <div class="datosH">
-                <h3>{{ $producto->nombre }}</h3>
-                <p>Precio: ${{ $producto->precio }}</p>
-            </div>
-        </div>
-    @endforeach
-</div>
-{{( $productos->links() )}}
+    <h1>Lista de Productos</h1>
+    <div class="contenedorH">
+        @foreach($productos as $producto)
+            @component('productos._components.producto', ['producto' => $producto])
+            @endcomponent
+        @endforeach
+    </div>
+    {{ $productos->links() }}
 @endsection
