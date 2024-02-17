77394725e5b7238fae51663f31cf5a48117c9955
Dario Soto <DarioSoto_97@hotmail.com>
2024-02-17 12:17:54 +0100

    Mas rutas

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', HomeController::class)->name('home');
 Route::controller(ProductoController::class)->group(function () {
     Route::get('/productos', 'index')->name('productos.index');
     Route::get('/productos/create',  'create')->name('productos.create')->middleware('admin');
-    Route::post('/productos', 'store')->name('productos.store')->middleware('auth');
+    Route::post('/productos', 'store')->name('productos.store')->middleware('admin');
     Route::post('/productos/filtro', 'filtro')->name('productos.filtro');
     Route::get('/productos/{id}', 'show')->name('productos.show');
 });
