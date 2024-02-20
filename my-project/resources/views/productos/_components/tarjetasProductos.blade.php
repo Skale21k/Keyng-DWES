3687174f965bf810725184b67230bfaabed61e84
Skale21k <zeos21k@gmail.com>
2024-02-20 20:09:07 +0100

    Arreglada la puta mierda de Sergio

--- my-project/resources/views/productos/_components/tarjetasProductos.blade.php
+++ my-project/resources/views/productos/_components/tarjetasProductos.blade.php
@@ -1,9 +1,9 @@
 <body class="body">
-    <div class="container">
-        <div class="images">
-            <img src="{{ $producto->imagen_url }}" alt="{{ $producto->nombre }}" class="img" />
+    <button type="button" class="btn btn-secondary"><a href="{{route('productos.index')}}">Volver</a></button>
+        <div class="images" >
+            <img src="{{ $producto->imagen_url }}" alt="{{ $producto->nombre }}" class="imgDetalle" />
         </div>
-        <div class="product">
+        <div class="product" style="left: 700px; top:200px">
             <p>{{ $producto->categoria }}</p>
             <h1>{{ $producto->nombre }}</h1>
             <h2>{{ $producto->precio }}€</h2>
@@ -11,7 +11,7 @@
             <form action="{{route('carrito.add')}}" method="post">
                 @csrf
                 <input type="hidden" name="id" value="{{$producto->id}}">
-                <input type="submit" name="btn" class="btn btn-success w-100" value="add carrito">
+                <input type="submit" name="btn" class="btn btn-success w-100" value="Añadir al carrito">
                 @admin
                     <button class="add">Botón de Administrador</button>
                 @endadmin
@@ -19,6 +19,5 @@
         </div>
 
 
-    </div>
 
 </body>
