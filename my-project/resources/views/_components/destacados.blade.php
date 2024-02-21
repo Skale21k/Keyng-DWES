67879b2a6a73638d41e158267b26a0541e6bb4c0
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-21 17:41:57 +0100

    ajusto las imagenes de destacados

--- my-project/resources/views/_components/destacados.blade.php
+++ my-project/resources/views/_components/destacados.blade.php
@@ -1,29 +1,31 @@
 <div class="item">
-    <img src="{{ $producto->imagen_url }}" class="foto">
+    <div class="foto">
+        <img src="{{ $producto->imagen_url }}" >
+    </div>
     <div class="content">
 
 
         <table width="100%" cellspacing="0">
             <tr>
-                <td colspan="2">{{$producto->nombre}}</td>
+                <td colspan="2">{{ $producto->nombre }}</td>
 
             </tr>
             <tr>
                 <td>Precio:</td>
-                <td>{{$producto->precio}}€</td>
+                <td>{{ $producto->precio }}€</td>
             </tr>
             <tr>
                 <td>Cantidad:</td>
                 <td>
                     <div class="quantity-selector">
-                    <select name="cantidad" id="cantidad">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                    </select>
+                        <select name="cantidad" id="cantidad">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                        </select>
                     </div>
                 </td>
                 <td id="#cantidad"> </td>
@@ -31,7 +33,8 @@
             <tr>
 
                 <td colspan="2" class="descripcion">
-                <a href="#" class="button">Añadir al carro<iconify-icon icon="uil:shopping-cart"></a></td>
+                    <a href="#" class="button">Añadir al carro<iconify-icon icon="uil:shopping-cart"></a>
+                </td>
             </tr>
         </table>
     </div>
