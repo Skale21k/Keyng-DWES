d524a648687340182592e90764fbaafbe1952008
Skale21k <zeos21k@gmail.com>
2024-02-27 00:54:58 +0100

    Seleccion de rol en la edicion de Usuarios para los Admins.

--- my-project/resources/views/usuarios/_components/register.blade.php
+++ my-project/resources/views/usuarios/_components/register.blade.php
@@ -1,15 +1,15 @@
 @php
-    use App\Models\User;
+use App\Models\User;
 
-    $modoCreacion = true;
-    if (isset($usuarios->nombre)) {
-        $modoCreacion = false;
-    }
+$modoCreacion = true;
+if (isset($usuarios->nombre)) {
+$modoCreacion = false;
+}
 
-    $rutaAction = route('usuarios.store');
-    if(!$modoCreacion){
-        $rutaAction = route('usuarios.update', $usuarios);
-    }
+$rutaAction = route('usuarios.store');
+if(!$modoCreacion){
+$rutaAction = route('usuarios.update', $usuarios);
+}
 
 @endphp
 
@@ -17,54 +17,70 @@
 
     @csrf
     @if(!$modoCreacion)
-        @method('put')
+    @method('put')
     @endif
 
     <!-- Name input -->
     <div class="form-outline mb-4">
-        <label class="form-label" for="registerName" >Nombre</label>
-        <input type="text" id="registerName" class="form-control" name="nombre" value="{{ old('nombre', $usuarios->nombre ?? '') }}" required/>
+        <label class="form-label" for="registerName">Nombre</label>
+        <input type="text" id="registerName" class="form-control" name="nombre"
+            value="{{ old('nombre', $usuarios->nombre ?? '') }}" required />
 
     </div>
 
     <!-- Email input -->
     <div class="form-outline mb-4">
         <label class="form-label" for="registerEmail">Email</label>
-        <input type="email" id="registerEmail" class="form-control" name="email" value="{{ old('email', $usuarios->email ?? '') }}" required/>
+        <input type="email" id="registerEmail" class="form-control" name="email"
+            value="{{ old('email', $usuarios->email ?? '') }}" required />
 
     </div>
 
     <!-- Direccion input -->
     <div class="form-outline mb-4">
         <label class="form-label" for="registerUsername">Dirección</label>
-        <input type="text" id="registerdirection" class="form-control" name="direccion" value="{{ old('direccion', $usuarios->direccion ?? '')}}" required/>
+        <input type="text" id="registerdirection" class="form-control" name="direccion"
+            value="{{ old('direccion', $usuarios->direccion ?? '')}}" @if(!$modoCreacion) @if($usuarios->rol == "user")
+        required @endif @endif/>
 
     </div>
+    @if(!$modoCreacion && Auth::user()->rol == "admin")
+        <!-- Rol input -->
+        <div class="form-outline mb-4">
+            <label class="form-label mb-2" for="registerRol">Rol</label>
+            <select class="form-select" id="registerRol" name="rol" required>
+                <option value="user" @if(old('rol', $usuarios->rol ?? '') == 'user') selected @endif>Usuario</option>
+                <option value="admin" @if(old('rol', $usuarios->rol ?? '') == 'admin') selected @endif>Administrador
+                </option>
+            </select>
+        </div>
+    @endif
 
-    
     <div class="form-outline mb-4">
         <label class="form-label" for="registerPassword">Contraseña</label>
-        <input type="password" id="registerPassword" class="form-control" name="password" @if($modoCreacion) required @endif/>
+        <input type="password" id="registerPassword" class="form-control" name="password" @if($modoCreacion) required
+            @endif />
 
     </div>
 
     <div class="form-outline mb-4">
         <label class="form-label" for="registerRepeatPassword">Repite contraseña</label>
-        <input type="password" id="registerRepeatPassword" class="form-control"/>
+        <input type="password" id="registerRepeatPassword" class="form-control" />
     </div>
 
     <div class="form-outline mb-4">
         <label class="form-label" for="registerImagen">Imagen</label>
-        <input type="file" id="imagen" class="form-control" name="imagen"/>
+        <input type="file" id="imagen" class="form-control" name="imagen" />
     </div>
-    
+
 
     <!-- Submit button -->
-    <button type="submit" id="registerButton" class="btn btn-primary btn-block mb-3">@if($modoCreacion) Registrarse @else Aplicar cambios @endif </button>
+    <button type="submit" id="registerButton" class="btn btn-primary btn-block mb-3">@if($modoCreacion) Registrarse
+        @else Aplicar cambios @endif </button>
 </form>
 
 <script>
-    document.getElementById('registerPassword').addEventListener('input', function() {
+    document.getElementById('registerPassword').addEventListener('input', function () {
         if (this.value.length > 0) {
             document.getElementById('registerRepeatPassword').required = true;
         } else {
