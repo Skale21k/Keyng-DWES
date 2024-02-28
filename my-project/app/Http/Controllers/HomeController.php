c56b096e1405d6e5b560025cbba6e5fbef003d86
Skale21k <zeos21k@gmail.com>
2024-02-28 23:30:02 +0100

    Arreglado Home.

--- my-project/app/Http/Controllers/HomeController.php
+++ my-project/app/Http/Controllers/HomeController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Producto;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        // categorias
-        $productosAlimentacion = Producto::where('categoria', 'alimentacion')->take(5)->get();
-        $productosHogar = Producto::where('categoria', 'Hogar')->take(5)->get();
+        // Obtener las categorías
+        $categoriaAlimentacion = Categoria::where('nombre', 'Alimentación')->first();
+        $categoriaHogar = Categoria::where('nombre', 'Hogar')->first();
 
+        // Obtener los productos asociados a las categorías
+        $productosAlimentacion = Producto::where('categoria_id', $categoriaAlimentacion->id)
+                                         ->take(5)
+                                         ->get();
+        $productosHogar = Producto::where('categoria_id', $categoriaHogar->id)
+                                  ->take(5)
+                                  ->get();
 
         return view('home', [
             'productosAlimentacion' => $productosAlimentacion,
