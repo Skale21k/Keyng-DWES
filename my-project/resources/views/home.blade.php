623d4b4a1b0f002de1f51c94c4af1a7a5958eae7
Alexandar Ivanov <alexandarivanov1470@gmail.com>
2024-02-29 00:54:47 +0100

    crando el fichero de cantidad.js dentro de assets/js/

--- my-project/resources/views/home.blade.php
+++ my-project/resources/views/home.blade.php
@@ -35,26 +35,5 @@
     @endforeach
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            // Función para aumentar y disminuir la cantidad
-            $('.quantity-right-plus').click(function (e) {
-                // Aumentar la cantidad
-                e.preventDefault();
-                var quantityInput = $(this).closest('.product-item').find('.input-number');
-                var quantity = parseInt(quantityInput.val());
-                quantityInput.val(quantity + 1);
-            });
-
-            $('.quantity-left-minus').click(function (e) {
-                // Disminuir la cantidad
-                e.preventDefault();
-                var quantityInput = $(this).closest('.product-item').find('.input-number');
-                var quantity = parseInt(quantityInput.val());
-                if (quantity > 1) {
-                    quantityInput.val(quantity - 1);
-                }
-            });
-        });
-    </script>
+    <script src="{{ asset('assets/js/cantidad.js') }}"></script>
 @endsection
