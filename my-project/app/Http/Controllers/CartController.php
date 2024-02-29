b0df2c3393e439649bf3bdb7f0e713d8054b3c89
Skale21k <zeos21k@gmail.com>
2024-02-29 22:45:53 +0100

    Ahora no se pueden añadir al carrito más productos de los que existen.

--- my-project/app/Http/Controllers/CartController.php
+++ my-project/app/Http/Controllers/CartController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Facades\Redirect;
 
 
 class CartController extends Controller
 {
     public function add(Request $request){
         $producto = Producto::find($request->id);
+        if($request->quantity[$producto->id] > $producto->unidades){
+            $errors = new MessageBag(['unidades' => ['No existen esas unidades para el producto actualmente, lo sentimos.']]);
+            return Redirect::back()->withErrors($errors);
+        }
         if(isset($request->quantity[$producto->id])){
             $qty = $request->quantity[$producto->id];
         } else{
--- my-project/resources/views/productos/index.blade.php
+++ my-project/resources/views/productos/index.blade.php
@@ -4,6 +4,11 @@
 @section('title', 'Lista de Productos')
 
 @section('content')
+@if ($error = $errors->first('unidades'))
+    <div class="alert alert-danger">
+        {{ $error }}
+    </div>
+@endif
 @component('productos._components.producto', ['productos' => $productos])
 @endcomponent
 @component('productos._components.menuLateral', ['categorias' => $categorias])
