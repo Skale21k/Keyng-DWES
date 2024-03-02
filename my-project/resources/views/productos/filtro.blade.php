74eb74aeb5390b11984cd5454ffa8dca3b924113
Skale21k <zeos21k@gmail.com>
2024-03-02 11:39:55 +0100

    Mensaje de error para las unidades en filtro.

--- my-project/resources/views/productos/filtro.blade.php
+++ my-project/resources/views/productos/filtro.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.plantilla')
 
 @section('content')
+@if ($error = $errors->first('unidades'))
+    <div class="alert alert-danger">
+        {{ $error }}
+    </div>
+@endif
 
     <h2>Resultados de la búsqueda para "{{ $nombre }}"</h2>
         @if($productos->isEmpty())
